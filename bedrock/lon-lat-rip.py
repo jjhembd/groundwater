8fe102e0aa87a31e8e11e523326f4479236da827
Jeshurun Hembd <jjhembd@gmail.com>
2020-02-18 16:18:29 -0600

    Update notebook explanation of Gaussian kernel interpolation

--- bedrock/lon-lat-rip.py
+++ bedrock/lon-lat-rip.py
@@ -3,10 +3,10 @@ import pandas
 def processFiles():
     # Read the main and rippability files, and merge them
     logs = pandas.read_csv('data/Reported_Well_Logs.csv')
-    rips = pandas.read_csv('data/avg_rippability_0-20.csv')
+    rips = pandas.read_csv('data/avg_rippability_0-50.csv')
     merge = logs.merge(rips, left_on='WELL_ID', right_on='SITE_ID')
 
-    outfile = 'data/lonlatrip_0-20.csv'
+    outfile = 'data/lonlatrip_0-50.csv'
     columns = ['SITE_ID', 'LONGITUDE', 'LATITUDE', 'RIPPABILITY']
     merge[columns].to_csv(outfile, encoding='utf-8', index=False)
 
